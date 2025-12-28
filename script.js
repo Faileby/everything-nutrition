c5c7a8ccfa02956f1bbd2fcdfc0b6ced5f4c2cf1
Faileby <anilbastuerk@gmail.com>
2025-12-28 16:52:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// ==========================
+// 👉 DATEN HIER EINTRAGEN 👈
+// ==========================
+
+const foods = [
+    {
+        name: "Apfel",
+        category: "Obst",
+        calories: 52,
+        protein: 0.3,
+        carbs: 14,
+        fat: 0.2,
+        fiber: 2.4,
+        vitaminC: 4.6,
+        iron: 0.1
+    },
+    {
+        name: "Brokkoli",
+        category: "Gemüse",
+        calories: 34,
+        protein: 2.8,
+        carbs: 7,
+        fat: 0.4,
+        fiber: 2.6,
+        vitaminC: 89,
+        iron: 0.7
+    }
+
+    // 👉 weitere Obst/Gemüse hier hinzufügen
+];
